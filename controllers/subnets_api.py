10f6526e92417238ebb158f788159182b63e4a8c
Eric Donohue <edonohue@brocade.com>
2017-03-03 11:51:01 -0500

    Commit

--- controllers/subnets_api.py
+++ controllers/subnets_api.py
@@ -16,6 +16,84 @@ class SubnetsApi(object):
         result = self.phpipam.api_send_request(path=uri, method='get')
         return result
 
+
+    def get_subnet_usage(self, subnet_id=''):
+        """ get subnet usage details based on ID """
+        uri = 'subnets/' + str(subnet_id) + '/usage/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnet_slaves(self, subnet_id=''):
+        """ get subnet slave details based on ID """
+        uri = 'subnets/' + str(subnet_id) + '/slaves/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnet_slaves_recursive(self, subnet_id=''):
+        """ get subnet recursive salves details based on ID """
+        uri = 'subnets/' + str(subnet_id) + '/slaves_recursive/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnet_addresses(self, subnet_id=''):
+        """ get list of addresses in subnet """
+        uri = 'subnets/' + str(subnet_id) + '/addresses/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def get_subnet_address(self, subnet_id='', address=''):
+        """ get list of addresses in subnet """
+        uri = 'subnets/' + str(subnet_id) + '/addresses/' + str(address) + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def get_subnet_first_free_address(self, subnet_id=''):
+        """ get first available addresss in subnet """
+        uri = 'subnets/' + str(subnet_id) + '/first_free/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def get_subnet_first_free_subnet(self, subnet_id='', mask=''):
+        """ get first available subnet in parent subnet """
+        uri = 'subnets/' + str(subnet_id) + '/first_subnet/' + str(mask) +'/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnet_free_subnets(self, subnet_id='', mask=''):
+        """ list available subnet in parent subnet """
+        uri = 'subnets/' + str(subnet_id) + '/all_subnets/' + str(mask) +'/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnet_custom_fields(self):
+        """ list subnet custom fields """
+        uri = 'subnets/custom_fields/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_subnets_cidr(self, subnet_cidr=''):
+        """ lists subnets based on CIDR notation """
+        uri = 'subnets/cidr/' + subnet_cidr + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def search_subnets_cidr(self, subnet_cidr=''):
+        """ searches for subnets based on CIDR notation """
+        uri = 'subnets/search/' + subnet_cidr + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
     def add_subnet(self, subnet='', mask='', section='',
                    permissions='', **kwargs):
         """ add new subnet """
@@ -49,6 +127,7 @@ class SubnetsApi(object):
             path=uri, method='post', payload=payload)
         return result
 
+
     def del_subnet(self, subnet_id=''):
         """ delete subnet """
         uri = 'subnets/' + str(subnet_id) + '/'
@@ -56,23 +135,15 @@ class SubnetsApi(object):
         return result
 
 
-    def list_subnets_cidr(self, subnet_cidr=''):
-        """ lists subnets based on CIDR notation """
-        uri = 'subnets/cidr/' + subnet_cidr
-        result = self.phpipam.api_send_request(path=uri, method='get')
+    def del_subnet_addresses(self, subnet_id=''):
+        """ delete subnet addresses """
+        uri = 'subnets/' + str(subnet_id) + '/truncate/'
+        result = self.phpipam.api_send_request(path=uri, method='delete')
         return result
 
 
-    def get_subnet_first_free_address(self, subnet_id=''):
-        """ get first available addresss in subnet """
-        uri = 'subnets/' + str(subnet_id) + '/first_free/'
-        result = self.phpipam.api_send_request(path=uri, method='get')
+    def del_subnet_permissions(self, subnet_id=''):
+        """ delete subnet permissions """
+        uri = 'subnets/' + str(subnet_id) + '/permissions/'
+        result = self.phpipam.api_send_request(path=uri, method='delete')
         return result
-
-
-    def list_subnet_addresses(self, subnet_id=''):
-        """ get list of addresses in subnet """
-        uri = 'subnets/' + str(subnet_id) + '/addresses/'
-        result = self.phpipam.api_send_request(path=uri, method='get')
-        return result
-        
