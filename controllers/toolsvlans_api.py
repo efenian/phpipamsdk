f3bf263fd0e4fb29aabce4511dbe889f82e47c34
Eric Donohue <edonohue@brocade.com>
2017-03-06 12:00:00 -0500

    Commit

--- controllers/toolsvlans_api.py
+++ controllers/toolsvlans_api.py
@@ -11,21 +11,21 @@ class ToolsVlansApi(object):
             self.phpipam = PhpIpamApi()
 
 
-    def list_vlans(self):
+    def list_tools_vlans(self):
         """ get vlan list """
         uri = 'tools/vlans/'
         result = self.phpipam.api_send_request(path=uri, method='get')
         return result
 
 
-    def get_vlan(self, vlan_id=''):
+    def get_tools_vlan(self, vlan_id=''):
         """ get vlan list """
         uri = 'tools/vlans/' + str(vlan_id) +'/'
         result = self.phpipam.api_send_request(path=uri, method='get')
         return result
 
 
-    def list_vlan_subnets(self, vlan_id=''):
+    def list_tools_vlan_subnets(self, vlan_id=''):
         """ get vlan subnet list """
         uri = 'tools/vlans/' + str(vlan_id) + '/subnets/'
         result = self.phpipam.api_send_request(path=uri, method='get')
--- controllers/vlans_api.py
+++ controllers/vlans_api.py
@@ -11,6 +11,48 @@ class VlansApi(object):
             self.phpipam = PhpIpamApi()
 
 
+    def list_vlans(self):
+        """ list vlans """
+        uri = 'vlans/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def get_vlan(self, vlan_id=''):
+        """ list vlans """
+        uri = 'vlans/' + str(vlan_id) + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_vlan_subnets(self, vlan_id=''):
+        """ list vlan subnets """
+        uri = 'vlans/' + str(vlan_id) + '/subnets/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_vlan_subnets_section(self, vlan_id='', section_id=''):
+        """ list vlan subnets section """
+        uri = 'vlans/' + str(vlan_id) + '/subnets/' + str(section_id) +'/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_vlan_custom_fields(self, vlan_id=''):
+        """ list vlan custom fields """
+        uri = 'vlans/' + str(vlan_id) + '/custom_fields/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def search_vlans(self, vlan_id='', vlan=''):
+        """ search vlans """
+        uri = 'vlans/' + str(vlan_id) + '/search/' + str(vlan) + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
     def add_vlan(self, name='', number='', **kwargs):
         """ add new vlan """
         payload = {
@@ -24,6 +66,23 @@ class VlansApi(object):
         uri = 'vlans/'
         result = self.phpipam.api_send_request(
             path=uri, method='post', payload=payload)
+        return result
+
+
+    def update_vlan(self, vlan_id='', **kwargs):
+        """ add new vlan """
+        payload = {}
+        if 'name' in kwargs:
+            payload['name'] = kwargs['name']
+        if 'number' in kwargs:
+            payload['number'] = kwargs['number']
+        if 'description' in kwargs:
+            payload['description'] = kwargs['description']
+        if 'domain_id' in kwargs:
+            payload['domainId'] = str(kwargs['domain_id'])
+        uri = 'vlans/' + str(vlan_id) + '/'
+        result = self.phpipam.api_send_request(
+            path=uri, method='patch', payload=payload)
         return result
 
 
