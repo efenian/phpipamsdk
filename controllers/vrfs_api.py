e9425d3ae06b1c207c2468f4c8ee608e02e2b3fa
Eric Donohue <edonohue@brocade.com>
2017-03-06 13:47:06 -0500

    Commit

--- controllers/vrfs_api.py
+++ controllers/vrfs_api.py
@@ -13,7 +13,28 @@ class VRFsApi(object):
 
     def list_vrfs(self):
         """ list vrfs """
-        uri = 'vrfs/'
+        uri = 'vrf/'
         result = self.phpipam.api_send_request(path=uri, method='get')
         return result
-        
+
+
+    def get_vrf(self, vrf_id=''):
+        """ get vrf """
+        uri = 'vrf/' + str(vrf_id) + '/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_vrf_subnets(self, vrf_id=''):
+        """ list vrfs subnets """
+        uri = 'vrf/' + str(vrf_id) + '/subnets/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+
+
+    def list_vrf_custom_fields(self):
+        """ list vrfs custom fields """
+        uri = 'vrf/custom_fields/'
+        result = self.phpipam.api_send_request(path=uri, method='get')
+        return result
+    
